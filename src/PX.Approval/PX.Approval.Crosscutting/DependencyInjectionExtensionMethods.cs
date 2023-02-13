5969709b20394edf2ab83a4b405b8a0b38fca918
Rodolpho Fernandes Orfenes de Castro <rodolphofernandes.orfenesdecastro.ext@bayer.com>
2023-02-13 17:17:00 -0300

    Adicionado validaçoes de autenticaçao via middleware

--- src/PX.Approval/PX.Approval.Crosscutting/DependencyInjectionExtensionMethods.cs
+++ src/PX.Approval/PX.Approval.Crosscutting/DependencyInjectionExtensionMethods.cs
@@ -3,10 +3,12 @@ using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PX.Approval.Application.Common.Interfaces;
+using PX.Approval.Application.Common.PipelineBehaviours;
 using PX.Approval.Domain.DomainObjects;
 using PX.Approval.Domain.Response;
 using PX.Approval.Infrastructure.Services.Crop;
 using PX.Approval.Infrastructure.Services.GoalsPlanning;
+using PX.Library.Common.OmegaServices;
 using System.Reflection;
 
 namespace PX.Approval.Crosscutting;
@@ -15,14 +17,23 @@ public static class DependencyInjectionExtensionMethods
 {
     private const string applicationProjectName = "PX.Approval.Application";
 
-    public static IServiceCollection AddOProjectServices(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddProjectDependencies(this IServiceCollection services, IConfiguration configuration)
     {
-        services.Configure<GoalsPlanningClientConfiguration>(configuration.GetSection(GoalsPlanningClientConfiguration.GoalsPlanningOptions));
-        services.Configure<CropClientConfiguration>(configuration.GetSection(CropClientConfiguration.CropClientOptions));
+        var assembly = Assembly.Load(applicationProjectName);
+        //services.AddAutoMapper(assembly);
+
         services.AddScoped<IResponse, Response>();
         services.AddScoped<IGoalsPlanningClient, GoalsPlanningGrpcClient>();
         services.AddScoped<ICropServiceClient, CropGrpcClient>();
+        services.AddHttpContextAccessor();
+        services.AddMediatr();
+        services.AddHttpContextAccessor();
 
+        #region "Pipeline"
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(AuthorizeBehaviour<,>));   //1st
+
+
+        #endregion
         return services;
     }
 
@@ -44,5 +55,22 @@ public static class DependencyInjectionExtensionMethods
 
         services.AddMediatR(assembly);
     }
+
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOmegaDependencyInjection(services => {
+            services.Configure<OmegaConfiguration>(configuration.GetSection(OmegaConfiguration.Omega));
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddApplicationConfigurations(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<GoalsPlanningClientConfiguration>(configuration.GetSection(GoalsPlanningClientConfiguration.GoalsPlanningOptions));
+        services.Configure<CropClientConfiguration>(configuration.GetSection(CropClientConfiguration.CropClientOptions));
+        return services;
+    }
+
 
 }
