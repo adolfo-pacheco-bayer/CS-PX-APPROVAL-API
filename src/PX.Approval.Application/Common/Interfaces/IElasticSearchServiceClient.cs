128b771f752f8251241fdcc598cd479f1f492cad
Rodolpho Fernandes Orfenes de Castro <rodolphofernandes.orfenesdecastro.ext@bayer.com>
2023-03-17 09:51:14 -0300

    Criando endpoint para retorna dados para o grafico de status

--- src/PX.Approval.Application/Common/Interfaces/IElasticSearchServiceClient.cs
+++ src/PX.Approval.Application/Common/Interfaces/IElasticSearchServiceClient.cs
@@ -5,5 +5,7 @@ namespace PX.Approval.Application.Common.Interfaces
     public interface IElasticSearchServiceClient
     {
         Task<List<PlanningElasticViewModel>> Get(Guid cropIntegrationId);
+
+        Task<List<PlanningElasticViewModel>> GetGraphicsByCropIntegrationId(string cropIntegrationId);
     }
 }
