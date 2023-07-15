d833ab9cf03429ff65c03e16190387c0125a48a4
Adolfo Sales Pacheco <adolfo.salespacheco.ext@bayer.com>
2023-07-15 16:20:09 -0300

    ajuste model totals

---
+++ src/PX.Approval.Application/ViewModel/TotalViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PX.Approval.Application.ViewModel
+{
+    public class TotalViewModel
+    {
+        public int Id { get; set; }
+        public Guid CropIntegrationId { get; set; }
+        public double FirstPeriod { get; set; }
+        public double SecondPeriod { get; set; }
+        public double Sellout { get; set; }
+        public double Price { get; set; }
+        public double Quimio { get; set; }
+        public double Seeds { get; set; }
+    }
+}
